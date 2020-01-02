4b0fde49c2aac7be7ab4574fc74ba3688d1c6fb8
Antonio Damato <anto.damato@gmail.com>
2020-01-02 09:20:07 +0000

    converted to binary tree

--- src/test/scala/com/algorithms/tree/TreeSuite.scala
+++ src/test/scala/com/algorithms/tree/TreeSuite.scala
@@ -6,54 +6,54 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TreeSuite extends FunSuite {
-  /* .......... 2 ....... */
+  /* .......... 10 ...... */
   /* ......... / \ ...... */
-  /* ........ 3 . 7 ..... */
+  /* ........ 7 . 12 .... */
   /* ....... / .......... */
   /* ...... 4 ........... */
   /* ..... / \ .......... */
-  /* .... 5 .. 4 ........ */
+  /* .... 3 . 6 ......... */
   private def createSimpleTree(): Tree = {
-    val treeL4_1 = Tree(4, None, None)
-    val treeL4_2 = Tree(5, None, None);
+    val treeL4_1 = Tree(6, None, None)
+    val treeL4_2 = Tree(3, None, None);
 
     val treeL3 = Tree(4, Some(treeL4_2), Some(treeL4_1));
 
-    val treeL2_1 = Tree(3, Some(treeL3), None);
-    val treeL2_2 = Tree(7, None, None);
+    val treeL2_1 = Tree(7, Some(treeL3), None);
+    val treeL2_2 = Tree(12, None, None);
 
-    val treeL1 = Tree(2, Some(treeL2_1), Some(treeL2_2));
+    val treeL1 = Tree(10, Some(treeL2_1), Some(treeL2_2));
     treeL1;
   }
 
-  /* .......... 4 .......... */
-  /* ........../.\.......... */
-  /* ........ 3 . 5 ........ */
-  /* ......../ \ . \........ */
-  /* ...... 2 . 7 . 6 ...... */
-  /* ..... / \ ... / \ ..... */
-  /* .... 3 . 8 . 8 . 8 .... */
-  /* ....... / \ ... / \ ... */
-  /* ...... 10  9 . 4 . 4 .. */
+  /* ............. 18 ............... */
+  /* ........... / ... \ ............ */
+  /* ........ 13 ....... 30 ......... */
+  /* ....... / \ ....... / .......... */
+  /* ...... 5 . 14 .... 20 .......... */
+  /* ..... / \ ...... / . \ ......... */
+  /* .... 2 . 8 .... 19 .. 25........ */
+  /* ....... / \ ........ / . \ ..... */
+  /* ...... 7 . 9 ...... 24 . 40 .... */
   private def createComplexTree(): Tree = {
-    val treeL5_1 = Tree(10, None, None);
+    val treeL5_1 = Tree(7, None, None);
     val treeL5_2 = Tree(9, None, None);
-    val treeL5_3 = Tree(4, None, None);
-    val treeL5_4 = Tree(4, None, None);
+    val treeL5_3 = Tree(24, None, None);
+    val treeL5_4 = Tree(40, None, None);
 
-    val treeL4_1 = Tree(3, None, None);
+    val treeL4_1 = Tree(2, None, None);
     val treeL4_2 = Tree(8, Some(treeL5_1), Some(treeL5_2));
-    val treeL4_3 = Tree(8, None, None);
-    val treeL4_4 = Tree(8, Some(treeL5_3), Some(treeL5_4));
+    val treeL4_3 = Tree(19, None, None);
+    val treeL4_4 = Tree(25, Some(treeL5_3), Some(treeL5_4));
 
-    val treeL3_1 = Tree(2, Some(treeL4_1), Some(treeL4_2));
-    val treeL3_2 = Tree(7, None, None);
-    val treeL3_3 = Tree(6, Some(treeL4_3), Some(treeL4_4));
+    val treeL3_1 = Tree(5, Some(treeL4_1), Some(treeL4_2));
+    val treeL3_2 = Tree(14, None, None);
+    val treeL3_3 = Tree(20, Some(treeL4_3), Some(treeL4_4));
 
-    val treeL2_1 = Tree(3, Some(treeL3_1), Some(treeL3_2));
-    val treeL2_2 = Tree(5, Some(treeL3_3), None);
+    val treeL2_1 = Tree(13, Some(treeL3_1), Some(treeL3_2));
+    val treeL2_2 = Tree(30, Some(treeL3_3), None);
 
-    val treeL1_1 = Tree(4, Some(treeL2_1), Some(treeL2_2));
+    val treeL1_1 = Tree(18, Some(treeL2_1), Some(treeL2_2));
     treeL1_1;
   }
 
